51cb86fe4f5010ff38ff2929f6f610c27e7bcbdb
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-10-05 17:25:32 +0200

    Update generateReadme.js

--- generateReadme.js
+++ generateReadme.js
@@ -25,6 +25,8 @@ async function generateReadme() {
 
 Welcome to my GitHub profile! I'm a passionate developer who loves exploring new technologies and building innovative solutions. This is a showcase of my skills, projects, and contributions.
 
+[![Typing SVG](https://readme-typing-svg.demolab.com?font=Fira+Code&pause=1000&width=435&lines=Go+touch+some+grass)](https://git.io/typing-svg)
+
 ## 🔧 Technologies & Languages
 
 ![Python](https://img.shields.io/badge/Python-3776AB?style=flat&logo=python&logoColor=white)
